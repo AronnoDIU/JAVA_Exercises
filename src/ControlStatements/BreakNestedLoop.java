33eccc414e8a46cee8b7685b172803e49dc58ce5
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-08-20 17:30:50 +0600

    Another BreakLoop added

---
+++ src/ControlStatements/BreakNestedLoop.java
@@ -0,0 +1,22 @@
+package ControlStatements;
+
+// Using break with nested loops.
+public class BreakNestedLoop {
+
+    public static void main(String[] args) {
+
+        for (var i = 0; i < 3; i++) {
+            System.out.print("Pass : " + i + " : ");
+            for (var j = 0; j < 100; j++) {
+                if (j == 10)
+                    break; // terminate loop if j is 10;
+                System.out.print(j + " ");
+            }
+            System.out.println();
+
+        }
+        System.out.println("Loop complete.");
+
+    }
+
+}
